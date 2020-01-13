b6710d5b3c48dced58322dd5f8670aa7cd12047a
Ronald Ordoñez <37679541+ronordonezq@users.noreply.github.com>
2020-01-13 13:41:36 -0500

    Creacion de menu de actividades

--- app/src/main/java/com/example/u1temaandroid_ejerciciospropuestos/MainActivity.java
+++ app/src/main/java/com/example/u1temaandroid_ejerciciospropuestos/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.u1temaandroid_ejerciciospropuestos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ir_calculadora(View view) {
+        startActivity(new Intent(this, Calculadora.class));
+    }
+
+    public void ir_formulario(View view) {
+        startActivity(new Intent(this, Formulario.class));
     }
 }
